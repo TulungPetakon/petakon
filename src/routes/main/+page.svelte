ecf38a251021170f276955ff1db5f7addc393bcf
AguzzTN54 <gusagusnanang@gmail.com>
2024-10-09 08:15:50 +0700

    Partners

--- src/routes/main/+page.svelte
+++ src/routes/main/+page.svelte
@@ -4,6 +4,7 @@
 	import Hero from './_hero.svelte';
 	import Highlight from './_highlight.svelte';
 	import Homestay from './_homestay.svelte';
+	import Partners from './_partners.svelte';
 	import Transport from './_transport.svelte';
 </script>
 
@@ -16,5 +17,7 @@
 <Homestay />
 
 <Transport />
+
+<Partners />
 
 <Footer />
---
+++ src/routes/main/_partners.svelte
@@ -0,0 +1,21 @@
+<script>
+	import p1 from '@images/partners/harapan-jaya.png?format=webp';
+	import p2 from '@images/partners/hotel-surakarta.png?format=webp';
+	import p3 from '@images/partners/ilio-guest-house.png?format=webp';
+	import p4 from '@images/partners/jaya-mandiri.png?format=webp';
+</script>
+
+<section class="pt-20 px-60">
+	<h2 class="font-semibold text-center text-xl">Trusted by:</h2>
+	<div class="flex pt-5 justify-center items-center">
+		{#each [p1, p2, p3, p4] as src}
+			<div class="px-2">
+				<img
+					{src}
+					alt="partner"
+					class="max-h-[100px] grayscale opacity-35 hover:grayscale-0 hover:opacity-100 transition-all duration-300"
+				/>
+			</div>
+		{/each}
+	</div>
+</section>
