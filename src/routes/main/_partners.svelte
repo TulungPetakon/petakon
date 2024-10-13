480267ae117f6424cae1899e1dd6ce3ef41615d9
AguzzTN54 <gusagusnanang@gmail.com>
2024-10-13 18:46:27 +0700

    More Partner

--- src/routes/main/_partners.svelte
+++ src/routes/main/_partners.svelte
@@ -1,14 +1,15 @@
 <script>
-	import p1 from '@images/partners/harapan-jaya.png?format=webp';
-	import p2 from '@images/partners/hotel-surakarta.png?format=webp';
-	import p3 from '@images/partners/ilio-guest-house.png?format=webp';
-	import p4 from '@images/partners/jaya-mandiri.png?format=webp';
+	import p0 from '@images/partners/tim-barista.png?format=webp&h=100';
+	import p1 from '@images/partners/harapan-jaya.png?format=webp&h=100';
+	import p2 from '@images/partners/hotel-surakarta.png?format=webp&h=100';
+	import p3 from '@images/partners/ilio-guest-house.png?format=webp&h=100';
+	import p4 from '@images/partners/jaya-mandiri.png?format=webp&h=100';
 </script>
 
 <section class="pt-20 px-[5%] md:px-[10%]">
 	<h2 class="font-semibold text-center md:text-xl">Trusted by:</h2>
 	<div class="flex pt-5 justify-center items-center">
-		{#each [p1, p2, p3, p4] as src}
+		{#each [p0, p1, p2, p3, p4] as src}
 			<div class="px-2">
 				<img
 					{src}
