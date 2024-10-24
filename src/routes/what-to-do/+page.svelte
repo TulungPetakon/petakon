79678c650b2c4448382a8277e970f25fbe34baf7
AguzzTN54 <gusagusnanang@gmail.com>
2024-10-25 00:44:01 +0700

    App and Todo routes

---
+++ src/routes/what-to-do/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	const load = async () => {
+		const post = await fetch('/post/what-to-do.md');
+		const text = await post.text();
+		return text;
+	};
+</script>
+
+<section class="px-[6%] py-10">
+	{#await load() then text}
+		<p>
+			{text}
+		</p>
+	{/await}
+</section>
