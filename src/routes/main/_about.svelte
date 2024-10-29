29edd8d1b7a51fd7cf04027fa01af25ab1f1ce46
AguzzTN54 <gusagusnanang@gmail.com>
2024-10-30 01:12:34 +0700

    About Section

---
+++ src/routes/main/_about.svelte
@@ -0,0 +1,51 @@
+<script>
+	import img from '@images/utils/ta.png';
+</script>
+
+<section class="flex pt-28 px-[5%] md:px-[6.5%] items-center flex-wrap">
+	<!-- Left -->
+	<div class="w-full sm:w-8/12 sm:text-right">
+		<div class="relative">
+			<span class="absolute bottom-full left-1/2 -translate-x-1/2 w-full">
+				Tak Kenal Maka Tak Sayang
+			</span>
+			<h2 class="font-bold text-4xl uppercase relative overflow-hidden">
+				<span class="relative inline-block py-1">
+					Sekilas tentang <u>Tulungagung </u>
+				</span>
+			</h2>
+		</div>
+
+		<div class="mt-10">
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil perferendis nulla alias
+				dignissimos temporibus iure aut facilis qui animi. Iure ab doloremque incidunt saepe quae
+				magnam minima. Natus, ratione? Veniam asperiores nobis voluptatum dicta, sit enim laudantium
+				consectetur, ab mollitia tempora deserunt doloribus veritatis impedit aut rem, sunt non. Id!
+			</p>
+			<p class="mt-4">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa in quaerat voluptate
+				perspiciatis! Facilis nihil similique impedit earum eos. Tempora doloribus voluptatum
+				perspiciatis minus nesciunt atque tempore ut quas possimus.
+			</p>
+		</div>
+
+		<div class="mt-10">
+			<a
+				href="/"
+				class="border-2 border-sky-600/50 px-4 py-2 rounded transition-all text-center
+      hover:border-sky-500 hover:bg-sky-500 hover:text-white"
+			>
+				<span> Baca Lebih Lengkap </span> <i class="fasl fa-arrow-right-long text-xl"></i>
+			</a>
+		</div>
+	</div>
+
+	<!-- Right -->
+	<div
+		class="flex items-center justify-center p-2 pt-20
+    sm:w-4/12 sm:pt-0"
+	>
+		<img src={img} alt="Tulungagung" class="w-4/6" />
+	</div>
+</section>
--- src/routes/main/main.svelte
+++ src/routes/main/main.svelte
@@ -1,13 +1,14 @@
 <script>
+	import About from './_about.svelte';
 	import Hero from './_hero.svelte';
 	import Highlight from './_highlight.svelte';
 	import Insight from './_insight.svelte';
-	import Partners from './_partners.svelte';
 	import Spotlight from './_spotlight.svelte';
 	import Trending from './_trending.svelte';
 	import Trips from './_trips.svelte';
 	// import Homestay from './_homestay.svelte';
 	// import Transport from './_transport.svelte';
+	// import Partners from './_partners.svelte';
 </script>
 
 <Hero />
@@ -20,10 +21,12 @@
 
 <Trips />
 
+<Insight />
+
+<About />
+
 <!-- <Homestay /> -->
 
 <!-- <Transport /> -->
 
-<Insight />
-
-<Partners />
+<!-- <Partners /> -->
