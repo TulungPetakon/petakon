d37010e8a8227116aa67fea28498f24dc5ad2e7a
AguzzTN54 <gusagusnanang@gmail.com>
2024-11-07 00:43:28 +0700

    Cloudflare Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +9,7 @@ const config = {
 		runes: true
 	},
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({}),
 		alias: {
 			$comp: path.resolve('./src/routes/_shared'),
 			$post: path.resolve('./src/posts'),
