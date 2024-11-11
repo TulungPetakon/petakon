a79eb06a5054c62ce26e0dd5bcdb998bc786d727
AguzzTN54 <gusagusnanang@gmail.com>
2024-11-11 21:09:47 +0700

    Instant ScrollTo without navigation

--- src/routes/_shared/scroll/ScrollPositionControl.svelte
+++ src/routes/_shared/scroll/ScrollPositionControl.svelte
@@ -2,9 +2,16 @@
 	import { afterNavigate } from '$app/navigation';
 	import { getContext } from 'svelte';
 
-	const { target, reset = false, scrollTo } = $props();
+	const { target, reset = false, scrollTo, instant } = $props();
 
 	const scrollControl = getContext('scrollControl');
+	$effect(() => {
+		if (!instant) return;
+		const scrollArea = document.querySelector(target);
+		scrollArea.scrollTo({ top: scrollTo, behavior: 'smooth' });
+		scrollControl({ reset: true });
+	});
+
 	afterNavigate(() => {
 		const scrollArea = document.querySelector(target);
 		if (reset) scrollArea.scrollTo({ top: 0 });
