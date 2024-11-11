7d4bba0d0d947864c8e71c3902818a785d53ef00
AguzzTN54 <gusagusnanang@gmail.com>
2024-11-12 04:13:42 +0700

    Remove vite-markdown-plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { imagetools } from 'vite-imagetools';
-import { plugin as MdPlugin } from 'vite-plugin-markdown';
 import { defineConfig } from 'vite';
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-	plugins: [imagetools(), sveltekit(), MdPlugin({ mode: 'html' })],
+	plugins: [imagetools(), sveltekit()],
 	build: {
 		chunkSizeWarningLimit: 350,
 		target: ['es2020']
