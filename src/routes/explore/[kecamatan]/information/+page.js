0175a8cf2babe08eeaa983e8c707ee869e33dc0f
AguzzTN54 <gusagusnanang@gmail.com>
2024-11-13 23:20:13 +0700

    Server Side Rendering

---
+++ src/routes/explore/[kecamatan]/information/+page.js
@@ -0,0 +1,16 @@
+const loadPost = async (k = '') => {
+	if (!k) return '';
+	try {
+		const { default: raw } = await import(`$post/kecamatan/${k}.md?raw`);
+		return raw;
+	} catch (e) {
+		console.error(e);
+		return '';
+	}
+};
+
+export const load = async ({ parent }) => {
+	const { district } = await parent();
+	const post = await loadPost(district.name);
+	return { district, post };
+};
--- src/routes/explore/[kecamatan]/information/+page.svelte
+++ src/routes/explore/[kecamatan]/information/+page.svelte
@@ -3,37 +3,26 @@
 	import { topbarHeight } from '$lib/stores/app-writable.svelte';
 
 	const { data } = $props();
-	const district = data.district.name;
-
-	const loadPost = async (k) => {
-		try {
-			const { default: raw } = await import(`$post/kecamatan/${k}.md?raw`);
-			return md(raw);
-		} catch (e) {
-			console.error(e);
-		}
-	};
+	const { html, toc } = md(data.post);
 </script>
 
 <section class="relative flex px-[5%] md:px-[10%] py-10 mb-20">
-	{#await loadPost(district) then { html, toc }}
-		<article class="pk-article basis-full md:basis-8/12">
-			{@html html}
-		</article>
+	<article class="pk-article basis-full md:basis-8/12">
+		{@html html}
+	</article>
 
-		<aside class="min-w-60 basis-4/12 hidden md:flex pl-10 relative">
-			<div class="ml-auto sticky right-0 h-fit pb-5" style="top:{$topbarHeight + 110}px;">
-				<h2 class="font-bold text-lg mb-2">Daftar Isi</h2>
-				<ol class="capitalize">
-					{#each toc as { slug, text }}
-						<li class="py-1 leading-5">
-							<a href="#{slug}" class="hover:underline hover:text-black inline-block">
-								{text}
-							</a>
-						</li>
-					{/each}
-				</ol>
-			</div>
-		</aside>
-	{/await}
+	<aside class="min-w-60 basis-4/12 hidden md:flex pl-10 relative">
+		<div class="ml-auto sticky right-0 h-fit pb-5" style="top:{$topbarHeight + 110}px;">
+			<h2 class="font-bold text-lg mb-2">Daftar Isi</h2>
+			<ol class="capitalize">
+				{#each toc as { slug, text }}
+					<li class="py-1 leading-5">
+						<a href="#{slug}" class="hover:underline hover:text-black inline-block">
+							{text}
+						</a>
+					</li>
+				{/each}
+			</ol>
+		</div>
+	</aside>
 </section>
