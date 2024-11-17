76f64ce05974817db551f3adda913fe1f27ad2fb
AguzzTN54 <gusagusnanang@gmail.com>
2024-11-17 23:13:16 +0700

    Essentials Page

---
+++ src/routes/explore/essential/+page.js
@@ -0,0 +1,12 @@
+const essentials = [
+	{ id: 3001, slug: 'batik-gm', name: 'Batik Gajah Mada', location: 'Mojosari, Kec. Kauman' },
+	{
+		id: 3002,
+		slug: 'intan-jaya',
+		name: 'Intan Jaya Resto - Pusat Oleh oleh',
+		location: 'Bolorejo, Kec. Kauman'
+	}
+];
+export const load = async () => {
+	return { essentials };
+};
--- src/routes/explore/essential/+page.svelte
+++ src/routes/explore/essential/+page.svelte
@@ -1,3 +1,69 @@
-<section class="px-[5%] md:px-[11%] py-20 bg-slate-100 w-full h-96">
-	<h1>Pelengkap Perjalanan</h1>
+<script>
+	import { getContext } from 'svelte';
+	import { preventDefault, stopPropagation } from '$lib/helpers/event-handler.helper';
+
+	const { data } = $props();
+	const { essentials } = data;
+
+	const getThumb = async (slug) => {
+		const { default: raw } = await import(`$images/essentials/${slug}.jpg`);
+		return raw;
+	};
+
+	const hello = (e) => {
+		console.log('hallo', e);
+	};
+	const mapToggle = getContext('mapToggle');
+	const openMap = () => mapToggle({ action: 'open', location: '' });
+</script>
+
+<section class="px-[calc(5%-0.75rem)] md:px-[calc(10%-0.75rem)] pt-5 pb-20 w-full">
+	<div
+		class="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:flex 2xl:grid-cols-none 2xl:flex-wrap"
+	>
+		{#each essentials as { location, name, slug }}
+			<div class="p-3 pb-5 2xl:w-72">
+				<a href="/accommodation/88390-front">
+					<div class="w-full aspect-[280/265] bg-gray-300 rounded-xl overflow-hidden relative">
+						{#await getThumb(slug) then img}
+							<img src={img} alt="Hotels" class="size-full" />
+						{/await}
+						<div class="absolute top-2 right-2">
+							<button
+								onclick={stopPropagation(preventDefault(hello))}
+								class="pk-button !p-0 w-8 text-3xl leading-none relative"
+								aria-label="Save for Later"
+								title="Save for Later"
+							>
+								<i class="fas fa-bookmark block relative text-gray-400/50"></i>
+								<i
+									class="fasl fa-bookmark block absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-white"
+								></i>
+							</button>
+						</div>
+					</div>
+					<div class="mt-2">
+						<h2
+							class="font-semibold text-lg leading-none text-overflow pt-2 pb-1 pr-5"
+							style="--line-number:1"
+						>
+							{name}
+						</h2>
+						<button class="flex">
+							<div class="text-gray-600 text-sm text-overflow">
+								<i class="fasl fa-location-dot"></i> <span> {location}</span>
+							</div>
+						</button>
+
+						<!-- <div class="my-1">
+								<span class="font-semibold">Rp999.999</span> <span>/malam</span>
+							</div> -->
+						<button class="block underline" onclick={stopPropagation(preventDefault(openMap))}>
+							Lihat di Peta</button
+						>
+					</div>
+				</a>
+			</div>
+		{/each}
+	</div>
 </section>
