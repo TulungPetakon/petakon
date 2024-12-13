5ed6d19f379e24d8845ea9dcab2fa851f5cd8395
AguzzTN54 <gusagusnanang@gmail.com>
2024-12-14 01:23:23 +0700

    Remove Unused

--- src/routes/main/main.svelte
+++ src/routes/main/main.svelte
@@ -6,9 +6,6 @@
 	import Spotlight from './_spotlight.svelte';
 	// import Trending from './_trending.svelte';
 	import Trips from './_trips.svelte';
-	// import Homestay from './_homestay.svelte';
-	// import Transport from './_transport.svelte';
-	// import Partners from './_partners.svelte';
 </script>
 
 <Hero />
@@ -26,9 +23,3 @@
 <About />
 
 <div class="mt-20"></div>
-
-<!-- <Homestay /> -->
-
-<!-- <Transport /> -->
-
-<!-- <Partners /> -->
