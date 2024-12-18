084380e838b7b9f59d84a888a7cdf9336e04718c
AguzzTN54 <gusagusnanang@gmail.com>
2024-12-18 08:12:57 +0700

    Nav Title

--- src/routes/_shared/menu/navlink.ts
+++ src/routes/_shared/menu/navlink.ts
@@ -1,9 +1,9 @@
 export const mainLinks = [
 	{
 		href: '/what-to-do',
-		title: 'Pertama Kali ke Tulungagung?',
+		title: 'Petunjuk',
 		id: 'first',
-		icon: 'fa-plane-arrival'
+		icon: 'fa-plane-circle-exclamation'
 	},
 	{ href: '/explore', title: 'Explore', id: 'explore', icon: 'fa-compass' },
 	{ href: '/planner', title: 'Rencana Perjalanan', id: 'planner', icon: 'fa-map-location-dot' }
