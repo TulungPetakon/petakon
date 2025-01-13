9c25b2fa59176f2814d557c3d36f4428981fefca
AguzzTN54 <gusagusnanang@gmail.com>
2025-01-13 21:03:06 +0700

    Remove Finished Events

--- src/routes/main/_events.svelte
+++ src/routes/main/_events.svelte
@@ -8,45 +8,45 @@
 	let splide: Splide;
 	let activeIndicator = $state(0);
 	const events = [
-		{
-			title: 'End Year Camp',
-			date: '31 Dec 2024',
-			location: 'Candi Urung',
-			tags: '',
-			periode: '',
-			gallery: [
-				{
-					url: 'end-year-camp',
-					type: 'picture'
-				}
-			]
-		},
-		{
-			title: 'BRILinkers Gayatri Tulungagung',
-			date: '31 Dec 2024',
-			location: 'Mbalong Kawuk',
-			tags: '',
-			periode: '',
-			gallery: [
-				{
-					url: 'bri-gayatri',
-					type: 'picture'
-				}
-			]
-		},
-		{
-			title: 'Tulungagung End Year 2024',
-			date: '31 Dec 2024',
-			location: 'Halaman Kantor Bupati Tulungagung',
-			tags: '',
-			periode: '',
-			gallery: [
-				{
-					url: 'save-the-rock',
-					type: 'picture'
-				}
-			]
-		},
+		// {
+		// 	title: 'End Year Camp',
+		// 	date: '31 Dec 2024',
+		// 	location: 'Candi Urung',
+		// 	tags: '',
+		// 	periode: '',
+		// 	gallery: [
+		// 		{
+		// 			url: 'end-year-camp',
+		// 			type: 'picture'
+		// 		}
+		// 	]
+		// },
+		// {
+		// 	title: 'BRILinkers Gayatri Tulungagung',
+		// 	date: '31 Dec 2024',
+		// 	location: 'Mbalong Kawuk',
+		// 	tags: '',
+		// 	periode: '',
+		// 	gallery: [
+		// 		{
+		// 			url: 'bri-gayatri',
+		// 			type: 'picture'
+		// 		}
+		// 	]
+		// },
+		// {
+		// 	title: 'Tulungagung End Year 2024',
+		// 	date: '31 Dec 2024',
+		// 	location: 'Halaman Kantor Bupati Tulungagung',
+		// 	tags: '',
+		// 	periode: '',
+		// 	gallery: [
+		// 		{
+		// 			url: 'save-the-rock',
+		// 			type: 'picture'
+		// 		}
+		// 	]
+		// },
 		{
 			title: 'Toeloengagoeng Koeliner Jaman Doeloe',
 			date: '27 - 28 April',
